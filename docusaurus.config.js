c90b2e3558bb9d4248521cb2e11c45af9dcd9d15
Nziie3 <106406461+Nzii3@users.noreply.github.com>
2022-09-25 14:20:16 -0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,8 +38,8 @@ const config = {
 
   themeConfig: {
     announcementBar: {
-      content: '<strong>Server Manager</strong> has been officially verified! <a href="/blog/verification-announcement">Check out our blog</a>!'
-      }
+      content: '<strong>Server Manager</strong> has been officially verified! <a href="/blog/verification-announcement">Check out our blog</a>!',
+      },
     docs: {
       sidebar: {
         autoCollapseCategories: true,
