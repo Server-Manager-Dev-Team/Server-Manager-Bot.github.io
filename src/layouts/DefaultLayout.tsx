9b4cce1b787e60eacc4d311b13d58e75c839270c
Nziie3 <106406461+Nzii3@users.noreply.github.com>
2022-09-25 11:07:39 -0700

    Update DefaultLayout.tsx

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -16,7 +16,7 @@ const DefaultLayout: React.FC<DefaultLayoutProps> = ({ title, description, child
             <Head>
                 <title>{ title } - { site.tagline }</title>
                 <meta name="description" content={ description } />
-                <meta property="og:description" content="Server Manager is a unique Discord bot that gives server moderators and admins easy and reliable ways to moderate the servers they care about, let Server Manager do the hard work for you." />
+                <meta property="og:description" content="Server Manager is a unique Discord bot that gives server moderators and admins easy and reliable ways to moderate servers, create interactive giveaways, create polls to vote on, and more. Let Server Manager do the hard work for you." />
                 <meta property="og:title" content="Server Manager" />
                 <meta property="og:image" content="https://imgur.com/BHyNRD2.png"/>
                 <meta name="twitter:card" content="summary_large_image"/>
